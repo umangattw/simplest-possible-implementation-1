18b7203ddc51b14b8e1f550397440082042f64f3
UMANG SINGHAL <umangsinghal@UMANGs-MacBook-Pro.local>
2021-01-04 18:00:08 +0530

    Test_1

---
+++ src/main/java/simplest/possible/implementation/PrimeFactors.java
@@ -0,0 +1,11 @@
+package simplest.possible.implementation;
+
+import java.util.List;
+
+import static java.util.Collections.emptyList;
+
+public class PrimeFactors {
+    public List<Integer> of(int number) {
+        return emptyList();
+    }
+}
